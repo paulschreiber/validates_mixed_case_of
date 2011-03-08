001532fa710580fde2112cd7ba36e1ac6d9ae105
Paul Schreiber <paulschreiber@gmail.com>
2011-03-08 11:12:59 -0500

    updated init.rb to work with rails 3

--- init.rb
+++ init.rb
@@ -2,4 +2,4 @@ Dir[File.join("#{File.dirname(__FILE__)}/config/locales/*.yml")].each do |locale
   I18n.load_path.unshift(locale)
 end
 
-require 'validates_mixed_case_of'
+require File.join(File.dirname(__FILE__), 'lib/validates_mixed_case_of')
